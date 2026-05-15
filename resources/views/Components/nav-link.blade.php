e6f2bf8b9b1b4c15fa866f342bac86690be05162
Gustavo <sasaeucomvoce@hotmail.com>
2026-05-14 21:44:19 -0300

    feat: homework - in case we need to render the nav-link as button

--- resources/views/Components/nav-link.blade.php
+++ resources/views/Components/nav-link.blade.php
@@ -1,7 +1,15 @@
-@props(['active' => false])
+@props(['active' => false, 'type' => 'a'])
 
-<a class="{{ $active ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md  px-3 py-2 text-sm font-medium"
-   aria-current="{{ $active ? 'page' : 'false' }}"
-    {{ $attributes }}
->{{ $slot }}
-</a>
+@if($type === 'a')
+    <a class="{{ $active ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md  px-3 py-2 text-sm font-medium"
+       aria-current="{{ $active ? 'page' : 'false' }}"
+        {{ $attributes }}
+    >{{ $slot }}
+    </a>
+@else
+    <button class="{{ $active ? 'bg-gray-950/50 text-white' : 'text-gray-300 hover:bg-white/5 hover:text-white' }} rounded-md  px-3 py-2 text-sm font-medium"
+            aria-current="{{ $active ? 'page' : 'false' }}"
+        {{ $attributes }}
+    >{{ $slot }}
+    </button>
+@endif
